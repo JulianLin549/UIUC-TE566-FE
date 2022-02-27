a64907251fe6d34651522b2158c85ca640246f5c
JulianLin549 <sres3416@gmail.com>
2022-02-27 04:34:41 -0600

    finish

--- src/components/BalanceSheet/BalanceSheet.tsx
+++ src/components/BalanceSheet/BalanceSheet.tsx
@@ -1,10 +1,146 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Box, Typography} from "@mui/material";
+import Grid from '@material-ui/core/Grid'
+
+import {Container} from "react-bootstrap";
+import axios from "axios";
+import {useHistory} from "react-router-dom";
+
 
 const BalanceSheet = () => {
+    const [bs, setBs] = useState<any>({});
+
+    useEffect(() => {
+        async function fetch() {
+            await handleGet()
+        }
+        fetch()
+    }, [])
+    const handleGet = async () => {
+        try {
+            const response = await axios.get(process.env.REACT_APP_BASE_URL + `/sheet/balance-sheet`);
+            console.log(response.data)
+            setBs(response.data);
+        } catch (e){
+            alert('get data failed')
+        }
+    };
     return (
-        <div>
-            bal
-        </div>
+        <Container>
+            <h1>Balance Sheet</h1>
+            <Box style={{ width: 800, margin: "24px auto" }}>
+                <Grid container style={{ margin: "10px auto" }}>
+                    <Grid item xs={6}>
+                        <Typography variant="body1" component="div">
+                            Assets
+                        </Typography>
+                        <hr/>
+                        <Box style={{ margin: "12px auto" , height: 100}}>
+                            <Grid container>
+                                <Grid item xs={6}>
+                                    <Typography variant="body2">Cash</Typography>
+                                    <Typography variant="body2">Account Receivable</Typography>
+                                    <Typography variant="body2">Inventory</Typography>
+                                    <Typography variant="body2">Total Current Asset</Typography>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <Typography variant="body2">{bs.cash}</Typography>
+                                    <Typography variant="body2">{bs.receivable}</Typography>
+                                    <Typography variant="body2">{bs.inventory}</Typography>
+                                    <Typography variant="body2">
+                                        {(parseFloat(bs.cash)+parseFloat(bs.receivable)+parseFloat(bs.inventory)).toFixed(2)}
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                        <Box style={{ margin: "12px auto" , height: 100}}>
+                            <Grid container>
+                                <Grid item xs={6}>
+                                    <Typography variant="body2">Building</Typography>
+                                    <Typography variant="body2">Equipment</Typography>
+                                    <Typography variant="body2">Total Fixed Assets</Typography>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <Typography variant="body2">{bs.building}</Typography>
+                                    <Typography variant="body2">{bs.equipment}</Typography>
+                                    <Typography variant="body2">{(parseFloat(bs.building)+parseFloat(bs.equipment)).toFixed(2)}</Typography>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                        <hr/>
+                        <Box style={{ margin: "12px auto" , height: 100}}>
+                            <Grid container>
+                                <Grid item xs={6}>
+                                    <Typography variant="body1">Total Assets</Typography>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <Typography variant="body1">
+                                        {(parseFloat(bs.cash)+parseFloat(bs.receivable)+parseFloat(bs.inventory)
+                                            +parseFloat(bs.building)+parseFloat(bs.equipment)).toFixed(2)}
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                        </Box>
+
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography variant="body1" component="div">
+                            Liability & Net Worth
+                        </Typography>
+                        <hr/>
+                        <Box style={{ margin: "12px auto" , height: 100}}>
+                            <Grid container>
+                                <Grid item xs={6}>
+                                    <Typography variant="body2">Payable</Typography>
+                                    <Typography variant="body2">Notes payable</Typography>
+                                    <Typography variant="body2">Total Current Liability</Typography>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <Typography variant="body2">{bs.payable}</Typography>
+                                    <Typography variant="body2">0.00</Typography>
+                                    <Typography variant="body2">{(parseFloat(bs.payable)).toFixed(2)}</Typography>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                        <Box style={{ margin: "12px auto" , height: 100}}>
+                            <Grid container>
+                                <Grid item xs={6}>
+                                    <Typography variant="body2">Mortgage</Typography>
+                                    <Typography variant="body2">Total Long Term Debt</Typography>
+                                    <hr/>
+                                    <Typography variant="body2">Total Liability</Typography>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <Typography variant="body2">{bs.mortgage}</Typography>
+                                    <Typography variant="body2">{(parseFloat(bs.mortgage)).toFixed(2)}</Typography>
+                                    <hr/>
+                                    <Typography variant="body2">
+                                        {(parseFloat(bs.payable)+parseFloat(bs.mortgage)).toFixed(2)}
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                        <hr/>
+                        <Box style={{ margin: "12px auto" , height: 100}}>
+                            <Grid container>
+                                <Grid item xs={6}>
+                                    <Typography variant="body1">Net Worth</Typography>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <Typography variant="body1">
+                                        {(parseFloat(bs.cash)+parseFloat(bs.receivable)+parseFloat(bs.inventory)
+                                            +parseFloat(bs.building)+parseFloat(bs.equipment)
+                                            -(parseFloat(bs.payable)+parseFloat(bs.mortgage))).toFixed(2)}
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Grid>
+                </Grid>
+
+            </Box>
+
+        </Container>
     );
 };
 
--- src/components/IncomeStatement/IncomeStatement.tsx
+++ src/components/IncomeStatement/IncomeStatement.tsx
@@ -1,10 +1,94 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Box, Typography} from "@mui/material";
+import Grid from '@material-ui/core/Grid'
+
+import {Container} from "react-bootstrap";
+import axios from "axios";
+import {useHistory} from "react-router-dom";
+
 
 const IncomeStatement = () => {
+    const history = useHistory();
+    const [is, setIs] = useState<any>({});
+    useEffect(() => {
+        async function fetch() {
+            await handleGet()
+        }
+        fetch()
+    }, [])
+    const handleGet = async () => {
+        try {
+            const response = await axios.get(process.env.REACT_APP_BASE_URL + `/sheet/income-statement`);
+            console.log(response.data)
+            setIs(response.data)
+        } catch (e){
+            alert('get data failed')
+        }
+    };
     return (
-        <div>
-            is
-        </div>
+        <Container>
+            <h1>Income Statement</h1>
+            <Box style={{ width: 600, margin: "24px auto" }}>
+                <Typography variant="h5" component="div">
+                    Sales
+                </Typography>
+                <Grid container>
+                    <Grid item xs={6}>
+                        <Typography variant="body2">Sales</Typography>
+                        <Typography variant="body2">COGS</Typography>
+                        <hr/>
+                        <Typography variant="body1">Gross Profit</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography variant="body2">{parseFloat(is.sales)}</Typography>
+                        <Typography variant="body2">{parseFloat(is.cogs)}</Typography>
+                        <hr/>
+                        <Typography variant="body1">{parseFloat(is.sales)-parseFloat(is.cogs)}</Typography>
+                    </Grid>
+                </Grid>
+            </Box>
+            <Box style={{ width: 600, margin: "24px auto" }}>
+                <Typography variant="h5" >
+                        Expenses
+                </Typography>
+                <Grid container>
+                    <Grid item xs={6}>
+                        <Typography variant="body2">Payroll</Typography>
+                        <Typography variant="body2">Bills</Typography>
+                        <Typography variant="body2">Annual Expense</Typography>
+                        <hr/>
+                        <Typography variant="body1">Total Expense</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography variant="body2">{parseFloat(is.payroll)}</Typography>
+                        <Typography variant="body2">{parseFloat(is.bills)}</Typography>
+                        <Typography variant="body2">0</Typography>
+                        <hr/>
+                        <Typography variant="body1">{parseFloat(is.payroll)+parseFloat(is.bills)}</Typography>
+                    </Grid>
+                </Grid>
+            </Box>
+            <Box style={{ width: 600, margin: "36px auto" }}>
+                <Grid container>
+                    <Grid item xs={6}>
+                        <Typography variant="body2">Other Income</Typography>
+                        <Typography variant="body2">Income Taxes (20%)</Typography>
+                        <hr/>
+                        <Typography variant="body1">Net Income</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography variant="body2">0</Typography>
+                        <Typography variant="body2">
+                            {(parseFloat(is.sales)-parseFloat(is.cogs)-parseFloat(is.payroll)-parseFloat(is.bills))*0.2}
+                        </Typography>
+                        <hr/>
+                        <Typography variant="body1">
+                            {(parseFloat(is.sales)-parseFloat(is.cogs)-parseFloat(is.payroll)-parseFloat(is.bills))*0.8}
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Box>
+        </Container>
     );
 };
 
